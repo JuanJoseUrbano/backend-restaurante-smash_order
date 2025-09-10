88b858e8151e5142313f1530c5e80aa56ad2f6f3
Juan Jose Urbano Perdomo <urbanoperdomoj@gmail.com>
2025-09-10 16:39:38 -0500

    feat(HU-106): setup Spring Boot project with initial dependencies for backend

---
+++ src/main/java/com/restaurant/SmashOrder/HelloWord.java
@@ -0,0 +1,12 @@
+package com.restaurant.SmashOrder;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWord {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
---
+++ src/test/java/com/restaurant/SmashOrder/SmashOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.SmashOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
